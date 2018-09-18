1baa9c9db692cc7b35316b26f1dd3698d3bd5128
Andrew Butterfield <Andrew.Butterfield@scss.tcd.ie>
2018-09-18 16:52:42 +0100

    first parsing example

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,27 @@
 module Main where
 
-import Lib
+import Language.Haskell.Parser
+import Language.Haskell.Pretty
 
 main :: IO ()
-main = someFunc
+main
+ = do putStrLn "Welcome to hreq"
+      let modstr = unlines [ "module X where"
+                           , ""
+                           , "f x  =  x + 1"
+                           , ""
+                           , "prop_f0       =  f 0 == 1"
+                           , "prop_f1       =  f 1 == 2"
+                           , "prop_f' x     =  f x > x"
+                           , "prop_f_inv x  =  f (x-1) == x"
+                           ]
+      putStrLn ("Module text:\n\n"++modstr)
+      case parseModule modstr of
+       ParseFailed loc str
+        -> putStrLn (unlines [show loc, str ])
+       ParseOk hsmod
+        -> do putStrLn "Module AST:\n"
+              putStrLn $ show hsmod
+              putStrLn "\nIsn't it pretty?\n"
+              putStrLn (prettyPrint hsmod)
+              putStrLn "Goodbye"
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,5 +1,5 @@
 module Lib
-    ( someFunc
+    (
     ) where
 
 someFunc :: IO ()
