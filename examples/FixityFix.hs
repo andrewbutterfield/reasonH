613d4eefed0f64f3241bb2cc9e9ccc3fecc2a3a2
Andrew Butterfield <Andrew.Butterfield@scss.tcd.ie>
2018-11-02 19:35:41 +0000

    got asolution, time to implement iy.....

--- examples/FixityFix.hs
+++ examples/FixityFix.hs
@@ -48,11 +48,59 @@ pfuse p (op:ops) (e1:e2:es)
  | p == prec op  =  pfuse p ops (B e1 op e2 : es)
  | otherwise  = (op:ops',e1:es') where (ops',es') = pfuse p ops (e2:es)
 
+pfusing :: Int -> ([String],[E]) -> ([String],[E])
+pfusing (-1) oes = oes
+pfusing p (ops,es) = pfusing (p-1) $ pfuse p ops es
+
+-- we assume everything is left-infix to start.
+fixfixity :: E -> E
+fixfixity a@(A _)  =  a
+fixfixity (B e1 op e2) = fix' (B (fixfixity e1) op (fixfixity e2))
+
+
+fix' (B b@(B e1 op1 e2) op2 e3)
+ | ass op1 == AR && ass op2 == AR && prec op1 == prec op2
+    = B e1 op1 (insSE op2 e2 e3 )
+fix' e = e
+
+insSE op2 (B e4 op3 e5) e3
+ | ass op3 == AR && prec op2 == prec op3
+   = B e4 op3 (insSE op2 e5 e3)
+insSE op2 e2 e3 = B e2 op2 e3
+
+
+
+
+fixit e = fixfixity $ head $ snd $ pfusing 9 $ split e
 
 {- ======= Examples ========= -}
-snoc = (B (B (B (A 3) ":" (A 2)) ":" (A 1)) ":" (A 0))
 
-amalt =  (B (B (B (B (A 1) "+" (A 2)) "*" (A 3)) "+" (A 4)) "*" (A 5))
+snoc = B (B (B (A 3) ":" (A 2)) ":" (A 1)) ":" (A 0) -- ((3:2):1):0
+
+-- wanted  3:(2:(1:0))
+
+-- fixit =  3:(2:(1:0))
+B 
+  (A 3)
+  ":"
+  (B
+    (A 2)
+    ":"
+    (B
+      (A 1)
+      ":"
+      (A 0)
+    )
+  )
+
+amalt =  B (B (B (B (A 1) "+" (A 2)) "*" (A 3)) "+" (A 4)) "*" (A 5)
+
+-- wanted (1+(2*3))+(4*5)
+
+-- fixfixity -.- pfuse 8..5 -.-  split amalt --> (1+(2*3))+(4*5)
 
 mix = wrap ["+",":","+",":","+",":","+",":"] $ map A $ reverse [0..8]
--- pfuse 8..5 $ split mix  --->   ((((8+7):(6+5)):(4+3)):(2+1)):0
+
+-- wanted (8+7):((6+5):((4+3):((2+1):0)))
+
+-- fixfixity -.- pfuse 8..5 -.-  split mix
